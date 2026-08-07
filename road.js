e592eb8e001daae7b1273726c4f132bf5d469f3f
abemisi <sahzul@gmail.com>
2026-08-08 00:03:05 +0800

    Update road.js

--- road.js
+++ road.js
@@ -1,1 +1,45 @@
+/* =====================================================
+   MISI RIDER V3.1
+   ROAD.JS
+===================================================== */
 
+/* =====================================================
+   ROAD DATA
+===================================================== */
+
+Game.road = {
+
+    offset: 0,
+
+    speed: 6
+
+};
+
+
+/* =====================================================
+   UPDATE ROAD
+===================================================== */
+
+function updateRoad(delta){
+
+    if(!Game.playing) return;
+
+    Game.road.offset += Game.speed * (delta / 16);
+
+    Game.ui.road.style.backgroundPositionY =
+        Game.road.offset + "px";
+
+}
+
+
+/* =====================================================
+   RESET ROAD
+===================================================== */
+
+function resetRoad(){
+
+    Game.road.offset = 0;
+
+    Game.ui.road.style.backgroundPositionY = "0px";
+
+}
