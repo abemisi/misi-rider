11035c9f64cd768d2e71a134cfeb8de36b27a0ed
abemisi <sahzul@gmail.com>
2026-08-08 00:41:04 +0800

    Update level.js

--- level.js
+++ level.js
@@ -1,1 +1,48 @@
+/* =====================================================
+   MISI RIDER V3.1
+   LEVEL.JS
+===================================================== */
 
+/* =====================================================
+   CHECK LEVEL
+===================================================== */
+
+function checkLevel(){
+
+    /* LEVEL 2 */
+
+    if(Game.score >= 500){
+
+        Game.level = 2;
+
+        Game.speed = 7;
+
+        Game.carSpawnDelay = 1000;
+
+    }
+
+    /* LEVEL 3 */
+
+    if(Game.score >= 1000){
+
+        Game.level = 3;
+
+        Game.speed = 8;
+
+        Game.carSpawnDelay = 850;
+
+    }
+
+    /* LEVEL 4 */
+
+    if(Game.score >= 1500){
+
+        Game.level = 4;
+
+        Game.speed = 9;
+
+        Game.carSpawnDelay = 700;
+
+    }
+
+}
